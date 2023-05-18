ed0b5a5efd9bcf42ab1e378984ba792f6ef44a66
husain-786 <sajjad.husain.s.p@gmail.com>
2023-05-18 23:29:11 +0530

    checking working or not

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,8 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
-    public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
-        // your code goes here
-    }
+	public DeluxePizza(Boolean isVeg) {
+		super(isVeg);
+		// your code goes here
+	}
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,33 @@
 package com.driver;
 
 public class Pizza {
+	private int price;
+	private Boolean isVeg;
+	private String bill;
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
+	public Pizza(Boolean isVeg) {
+		this.isVeg = isVeg;
+		// your code goes here
+	}
 
-    public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
-    }
+	public int getPrice() {
+		return this.price;
+	}
 
-    public int getPrice(){
-        return this.price;
-    }
+	public void addExtraCheese() {
+		// your code goes here
+	}
 
-    public void addExtraCheese(){
-        // your code goes here
-    }
+	public void addExtraToppings() {
+		// your code goes here
+	}
 
-    public void addExtraToppings(){
-        // your code goes here
-    }
+	public void addTakeaway() {
+		// your code goes here
+	}
 
-    public void addTakeaway(){
-        // your code goes here
-    }
-
-    public String getBill(){
-        // your code goes here
-        return this.bill;
-    }
+	public String getBill() {
+		// your code goes here
+		return this.bill;
+	}
 }
